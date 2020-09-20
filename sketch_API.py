e9030e46f5ee68c417144828d192031b9e72ffbe
Daniel Anderson <43540934+Danderson123@users.noreply.github.com>
2020-09-20 10:00:40 -0400

    Add files via upload

--- sketch_API.py
+++ sketch_API.py
@@ -4,6 +4,20 @@ from flask import Flask, render_template
 
 app = Flask("__main__")
 
+#Configure
+app.config['UPLOAD_EXTENSIONS'] = ['.fna', '.fasta', '.fa', '.txt']
+app.config['UPLOAD_PATH'] = 'uploads'
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    render_template('sketch.html')
+    ```Retrieve and save file uplaoded to uploader.html```
+
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        isolate_name = uploaded_file.read().decode("utf-8").split("\n")[0].replace('>', '')
+
+        uploaded_file = request.files['file']
+        uploaded_file.save(os.path.join('uploads', secure_filename(uploaded_file.filename)))
+        return render_template('sketch.html')
+    else:
+        return render_template('uploader.html')

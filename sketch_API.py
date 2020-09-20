d865e1ddf9b1e573b9dfc4b06913cdb8a8727d03
Daniel Anderson <43540934+Danderson123@users.noreply.github.com>
2020-09-20 09:52:08 -0400

    Update sketch_API.py

--- sketch_API.py
+++ sketch_API.py
@@ -1,3 +1,5 @@
+```Renders sketch.html template that runs sketch.js. Sequence uploader saves to "uploads", and will be retrieved and read by sketch.js```
+
 from flask import Flask, render_template
 
 app = Flask("__main__")

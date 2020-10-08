25dfaa49e15a7c30d1dc73fa6f9bcc50f811f14d
Daniel Anderson <43540934+Danderson123@users.noreply.github.com>
2020-10-08 15:50:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 //import launchWorker from './launchWorker'
 import WebWorker from './components/setup';
 import worker from './components/test.worker';
+//import worker from './components/flaskComponent';
 import DropZone from './components/DropZone';
 import Loading from './components/Loading'
 import ClusterResult from './components/clusterResult'
@@ -23,9 +24,30 @@ function App() {
   useEffect(() => {
     sketchWork.addEventListener("message", event => {
       console.log("WebWorker works!");
-      console.log('Cluster: ' + event.data);
+      console.log('Sketch: ' + event.data);
+
+      var data = new FormData();
+      const payload = {
+          bbits: "14",
+          sketchsize64: "156",
+          kmers: "14"
+      };
+      data.append("jsonsketch", JSON.stringify(payload));
+      
+      console.log('Posting sketch to Flask!');
+      fetch("http://localhost:5000/upload", {
+        method: 'POST',
+        mode: 'cors',
+        headers : { 
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload),
+      }).then((response) => response.json()).then((responseJson) => {
+        console.log(responseJson);
+        setCluster(responseJson);
+      })
       setLoading(false)
-      setCluster(event.data)
     });
   }, [sketchWork, setLoading, setCluster]); 
 

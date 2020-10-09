0610b0544a9537886f3585b4ecef207e98b04110
Daniel Anderson <43540934+Danderson123@users.noreply.github.com>
2020-10-09 16:28:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import './App.css';
-import WebWorker from './components/setup';
-import worker from './components/test.worker';
+//import WebWorker from './components/setup';
+//import worker from './components/test.worker';
 import DropZone from './components/DropZone';
 import Loading from './components/Loading'
 import ClusterResult from './components/clusterResult'
+import worker from 'workerize-loader!./components/test.worker'; // eslint-disable-line import/no-webpack-loader-syntax
+
 
 function App() {
   const [loading, setLoading] = useState(false) //define loading state
+  const [sketch, setSketch] = useState(null) //define sketch state
   const [display, setCluster] = useState(null) //define result state
 
-  let sketchWork = new WebWorker(worker);  // Declare worker
-  // Constants declaration
   const onDrop = useCallback(acceptedFiles => {
-      setLoading(true)
-      console.log(loading);
-      sketchWork.postMessage(acceptedFiles);
-      console.log("Sequence posted!");
-    }, [sketchWork, setLoading]); //Submit sequence to webworker for sketching
+    setLoading(true)
+    const workerInstance = worker();
+    //Spawn worker, post file and retrieve response  
+    workerInstance.sketchWorker(acceptedFiles); //call submit to webworker
+    //Submit sequence to webworker for sketching
 
-  useEffect(() => {
-    sketchWork.addEventListener("message", event => {
+    workerInstance.addEventListener('message', (message) => {
       console.log("WebWorker works!");
-      console.log('Sketch: ' + event.data);
+      console.log('Sketch: ' + message.data);
+      setSketch(message.data);
 
       const payload = {
-          bbits: "14",
-          sketchsize64: "156",
-          kmers: "14"
-      };
+        bbits: "14",
+        sketchsize64: "156",
+        kmers: "14"
+        };
       
       console.log('Posting sketch to Flask!');
       fetch("http://localhost:5000/upload", {
@@ -37,16 +38,16 @@ function App() {
         headers : { 
           'Accept': 'application/json',
           'Content-Type': 'application/json'
-        },
+         },
         body: JSON.stringify(payload),
-      }).then((response) => response.json()).then((responseJson) => {
-        console.log(responseJson);
-        setCluster(responseJson);
-        setLoading(false);
-        console.log("Flask delay complete!")
-      })
+        }).then((response) => response.json()).then((responseJson) => {
+      console.log(responseJson);
+      setCluster(responseJson);
+      setLoading(false);
+      console.log("Flask delay complete!");
+      });
     });
-  }, [sketchWork, setLoading, setCluster]); //Recieve sketch, post to Flask and recieve response from Flask
+  }, [setLoading, setSketch, setCluster]); //Recieve sketch, post to Flask and recieve response from Flask
 
   return (
     <main className="App">

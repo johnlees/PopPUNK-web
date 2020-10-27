ee20322ecb8b895a56cab6e44f026a9efa88680a
Danderson123 <danielanderson1@hotmail.com>
2020-10-27 15:54:19 +0000

    Working emscripten filesystem

--- public/worker.js
+++ public/worker.js
@@ -7,7 +7,7 @@ onmessage = function(message) {
     console.log(f);
 
     module.FS.mkdir('/working');
-    module.FS.mount(module.WORKERFS, { files: [f] }, '/working');
+    module.FS.mount(module.FS.filesystems.WORKERFS, { files: [f] }, '/working');
     console.log('sketch result: ' + module.sketch('/working/' + f.name, 15, 27, 2, 14, 156, false, true));
   });
 };
